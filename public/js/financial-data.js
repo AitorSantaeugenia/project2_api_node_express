832b4b2978a2065f11ba3437143df604ec722b28
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-15 14:52:31 +0200

    fixed issues with charts

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -28,8 +28,8 @@ window.onload = () => {
 					{
 						label: 'Value of coin',
 						data: prices,
-						backgroundColor: [ 'rgba(167, 235, 211, 0.5)' ],
-						borderColor: [ 'rgba(38, 199, 131, 1)' ],
+						backgroundColor: 'rgba(167, 235, 211, 0.5)',
+						borderColor: 'rgba(38, 199, 131, 1)',
 						borderWidth: 2
 					}
 				]
@@ -81,8 +81,8 @@ window.onload = () => {
 						{
 							label: 'Value of coin',
 							data: prices,
-							backgroundColor: [ 'rgba(167, 235, 211, 0.5)' ],
-							borderColor: [ 'rgba(38, 199, 131, 1)' ],
+							backgroundColor: 'rgba(167, 235, 211, 0.5)',
+							borderColor: 'rgba(38, 199, 131, 1)',
 							borderWidth: 1
 						}
 					]
@@ -140,8 +140,8 @@ window.onload = () => {
 						{
 							label: [ 'Value of coin' ],
 							data: prices,
-							backgroundColor: [ 'rgba(167, 235, 211, 0.5)' ],
-							borderColor: [ 'rgba(38, 199, 131, 1)' ],
+							backgroundColor: 'rgba(167, 235, 211, 0.5)',
+							borderColor: 'rgba(38, 199, 131, 1)',
 							borderWidth: 1
 						}
 					]
@@ -166,102 +166,102 @@ window.onload = () => {
 		});
 	});
 	//--------------------------------------------------------------------------------
-	// FAKE API
-	const typeChartButton = document.getElementById('typeChart');
-	typeChartButton.addEventListener('change', () => {
-		let typeChart = document.getElementById('typeChart').value;
-		let canvas2 = document.getElementById('myChartTwo').getContext('2d');
-		const url2 = 'https://api.jsonbin.io/b/613feeee9548541c29b16a62/1';
-		axios.get(url2).then((responseFromAPI) => {
-			//console.log(responseFromAPI.data.data);
-			if (typeChart == 'line' || typeChart == 'radar') {
-				bgcolor = 'rgba(167, 235, 211, 0.5)';
-				bdColor = 'rgba(38, 199, 131, 1)';
-			} else {
-				bgcolor = [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
-				];
-				bdColor = [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
-				];
-			}
+	// // FAKE API
+	// const typeChartButton = document.getElementById('typeChart');
+	// typeChartButton.addEventListener('change', () => {
+	// 	let typeChart = document.getElementById('typeChart').value;
+	// 	let canvas2 = document.getElementById('myChartTwo').getContext('2d');
+	// 	const url2 = 'https://api.jsonbin.io/b/613feeee9548541c29b16a62/1';
+	// 	axios.get(url2).then((responseFromAPI) => {
+	// 		//console.log(responseFromAPI.data.data);
+	// 		if (typeChart == 'line' || typeChart == 'radar') {
+	// 			bgcolor = 'rgba(167, 235, 211, 0.5)';
+	// 			bdColor = 'rgba(38, 199, 131, 1)';
+	// 		} else {
+	// 			bgcolor = [
+	// 				'rgba(255, 99, 132, 0.2)',
+	// 				'rgba(54, 162, 235, 0.2)',
+	// 				'rgba(255, 206, 86, 0.2)',
+	// 				'rgba(75, 192, 192, 0.2)',
+	// 				'rgba(153, 102, 255, 0.2)',
+	// 				'rgba(255, 159, 64, 0.2)'
+	// 			];
+	// 			bdColor = [
+	// 				'rgba(255, 99, 132, 1)',
+	// 				'rgba(54, 162, 235, 1)',
+	// 				'rgba(255, 206, 86, 1)',
+	// 				'rgba(75, 192, 192, 1)',
+	// 				'rgba(153, 102, 255, 1)',
+	// 				'rgba(255, 159, 64, 1)'
+	// 			];
+	// 		}
 
-			//console.log(typeChart);
-			//Get key
-			const labels = Object.keys(responseFromAPI.data.data);
-			const prices = labels.map((key) => responseFromAPI.data.data[key]);
-			// context.clearRect(0, 0, canvas2.width, canvas2.height);
-			// context.clearRect(0, 0, 1200, 1200);
+	// 		//console.log(typeChart);
+	// 		//Get key
+	// 		const labels = Object.keys(responseFromAPI.data.data);
+	// 		const prices = labels.map((key) => responseFromAPI.data.data[key]);
+	// 		// context.clearRect(0, 0, canvas2.width, canvas2.height);
+	// 		// context.clearRect(0, 0, 1200, 1200);
 
-			//Chart.defaults.font.size = 16;
-			let myChart = new Chart(canvas2, {
-				type: typeChart,
-				data: {
-					labels,
-					datasets: [
-						{
-							label: 'TOP10 Crypto',
-							data: prices,
-							backgroundColor: bgcolor,
-							borderColor: bdColor,
-							borderWidth: 1
-						}
-					]
-				},
-				options: {
-					responsive: true,
-					//add this to read keys
-					maintainAspectRatio: true,
-					scales: {
-						y: {
-							beginAtZero: true
-						}
-					},
-					plugins: {
-						legend: {
-							labels: {
-								// This more specific font property overrides the global property
-								font: {
-									size: 60
-								}
-							}
-						}
-					}
-				}
-			});
-		});
-	});
+	// 		//Chart.defaults.font.size = 16;
+	// 		let myChart = new Chart(canvas2, {
+	// 			type: typeChart,
+	// 			data: {
+	// 				labels,
+	// 				datasets: [
+	// 					{
+	// 						label: 'TOP10 Crypto',
+	// 						data: prices,
+	// 						backgroundColor: bgcolor,
+	// 						borderColor: bdColor,
+	// 						borderWidth: 1
+	// 					}
+	// 				]
+	// 			},
+	// 			options: {
+	// 				responsive: true,
+	// 				//add this to read keys
+	// 				maintainAspectRatio: true,
+	// 				scales: {
+	// 					y: {
+	// 						beginAtZero: true
+	// 					}
+	// 				},
+	// 				plugins: {
+	// 					legend: {
+	// 						labels: {
+	// 							// This more specific font property overrides the global property
+	// 							font: {
+	// 								size: 60
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		});
+	// 	});
+	// });
 };
-// Graph for currencies
-const clickToPdf = document.getElementById('downloadPdf');
-//var clickToPdf = document.getElementById('downloadPdf');
-let canvas = document.getElementById('myChartTwo');
-clickToPdf.addEventListener('click', () => {
-	canvas.toBlob(function(blob) {
-		let pdf = new jsPDF('p', 'px', [ 400, 400 ]);
-		pdf.addImage(canvas, 0, 0, 400, 400);
-		pdf.save('chartTopCurrency.pdf');
-	});
-});
+// // Graph for currencies
+// const clickToPdf = document.getElementById('downloadPdf');
+// //var clickToPdf = document.getElementById('downloadPdf');
+// let canvas = document.getElementById('myChartTwo');
+// clickToPdf.addEventListener('click', () => {
+// 	canvas.toBlob(function(blob) {
+// 		let pdf = new jsPDF('p', 'px', [ 400, 400 ]);
+// 		pdf.addImage(canvas, 0, 0, 400, 400);
+// 		pdf.save('chartTopCurrency.pdf');
+// 	});
+// });
 
 // Graph to download
-const clickToPdf2 = document.getElementById('downloadPdf2');
+const clickToPdfTwo = document.getElementById('downloadPdf2');
 //var clickToPdf = document.getElementById('downloadPdf');
-let canvas2 = document.getElementById('myChart');
-clickToPdf2.addEventListener('click', () => {
-	canvas2.toBlob(function(blob) {
+let canvasTwo = document.getElementById('myChart');
+clickToPdfTwo.addEventListener('click', () => {
+	canvasTwo.toBlob(function(blob) {
 		let pdf = new jsPDF('p', 'px', [ 400, 400 ]);
-		pdf.addImage(canvas2, 0, 0, 400, 400);
+		pdf.addImage(canvasTwo, 0, 0, 400, 400);
 		pdf.save('chartCurrentCoin.pdf');
 	});
 });
